57f041bd2633252b6c71178b2afff8e3edb27c0f
Filip Copija <filip.copija1@gmail.com>
2023-01-17 18:44:49 +0100

    Subscriber

---
+++ Observer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Observer;
+
+var sub1 = new Subscriber("Filip");
+var sub2 = new Subscriber("Kacper");
+var sub3 = new Subscriber("Seba");
+var sub4 = new Subscriber("Bartek");
+
+var publisher = new Publisher();
+
+publisher.Subscribe(sub1);
+publisher.Subscribe(sub3);
+
+publisher.Notify("PlayStation 5");
+
+publisher.Unsubscribe(sub1);
