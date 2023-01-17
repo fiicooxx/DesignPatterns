6d5332d1b1f02fa6b350ae2cc66a6db624b4368b
Filip Copija <filip.copija1@gmail.com>
2023-01-17 15:40:32 +0100

    AbstractFactory

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,9 @@
+﻿using AbstractFactory;
+
+var uiWinApp = new Application(new WindowsFactory());
+uiWinApp.RwnderUI();
+
+Console.WriteLine();
+
+var uiMacApp = new Application(new MacFactory());
+uiMacApp.RwnderUI();
