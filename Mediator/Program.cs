193ad55fec5f6edf04f6da4b69067b90697e2a7c
Filip Copija <filip.copija1@gmail.com>
2023-01-17 20:05:58 +0100

    Mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,9 @@
+﻿using Mediator;
+
+Button submitButton = new Button();
+Checkbox clientType = new Checkbox();
+
+new RegisterClientView(clientType, submitButton);
+
+submitButton.Click();
+clientType.Select();
