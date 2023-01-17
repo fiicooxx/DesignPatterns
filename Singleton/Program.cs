ac00387af13cba02213ccd50b60827ef7079b713
Filip Copija <filip.copija1@gmail.com>
2023-01-17 16:35:49 +0100

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton;
+
+var config1 = Configuration.GetInstance();
+var config2 = Configuration.GetInstance();
+
+if (ReferenceEquals(config1, config2))
+{
+    Console.WriteLine("Configuration is a singleton");
+}
