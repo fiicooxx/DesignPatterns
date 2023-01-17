c42ea25ff49ffe5dc2f4217956326280ce107080
Filip Copija <filip.copija1@gmail.com>
2023-01-18 00:11:26 +0100

    TemplateMethod

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using TemplateMethod;
+
+var pdfGenerator = new PdfGenerator();
+Console.WriteLine("** PDF **");
+pdfGenerator.GenerateReport();
+
+var csvGenerator = new CsvGenerator();
+Console.WriteLine("** CSV **");
+csvGenerator.GenerateReport();
+
+var excelGenerator = new ExcelGenerator();
+Console.WriteLine("** EXCEL **");
+excelGenerator.GenerateReport();
