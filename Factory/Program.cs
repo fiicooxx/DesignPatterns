7136a5fe8ce25a6a09c5839b5ff00cbdefd73e90
Filip Copija <filip.copija1@gmail.com>
2023-01-17 15:20:55 +0100

    Factory

---
+++ Factory/Program.cs
@@ -0,0 +1,14 @@
+﻿using Factory;
+
+ShapeFactory shapeFactory = new ShapeFactory();
+
+var circle = shapeFactory.CreateShape(ShapeType.Circle);
+circle.Render();
+
+var triangle = shapeFactory.CreateShape(ShapeType.Triangle);
+triangle.Render();
+
+var square = shapeFactory.CreateShape(ShapeType.Square);
+square.Render();
+
+
---
+++ Factory/ShapeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class ShapeFactory
+    {
+        public Shape CreateShape(ShapeType shapeType)
+        {
+            switch (shapeType)
+            {
+                case ShapeType.Circle:
+                    return new Circle();
+                case ShapeType.Square:
+                    return new Square();
+                case ShapeType.Triangle:
+                    return new Triangle();
+                default:
+                    throw new Exception($"Shape type {shapeType} is not handled!");
+            }
+            
+        }
+    }
+}
