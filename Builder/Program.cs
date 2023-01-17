93987363652dc3723a1c62f4a1ade9d1b74b9e15
Filip Copija <filip.copija1@gmail.com>
2023-01-17 14:52:42 +0100

    Builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Builder;
+
+var builder = new InvoiceBuilder();
+
+Invoice invoice = builder
+.SetDate(new DateTime(2023, 1, 17))
+.SetInvoiceNumber("A1")
+.SetVendor("Google")
+.SetVendee("Vendee")
+.SetLineItems(new List<string>() { "Line item1", "Line item2" })
+.Build();
+
+Console.WriteLine(invoice.Vendee);
+Console.WriteLine(invoice.Vendor);
